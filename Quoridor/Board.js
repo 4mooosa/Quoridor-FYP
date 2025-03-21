ae89e3ae06ca37fc9b63c21d4c8e6dd4749e6d27
Muhammad Moosa <mooosa4@outlook.com>
2025-03-21 23:54:02 +0000

    Updated createBoard() method

--- Quoridor/Board.js
+++ Quoridor/Board.js
@@ -1,5 +1,22 @@
 class Board {
-    constructor() {
-        this.board = [];
+    constructor(boardSize) {
+        this.boardSize = boardSize;
+        this.board = this.createBoard(this.boardSize);
+        
+        
+        console.log(`Hello`)
+    }
+
+    createBoard(boardSize) {
+        board = [];
+
+        for(var i = 0; i < this.boardSize; i++) {
+            row = [];
+            for(var i = 0; i < this.boardSize; i++) {
+                row.push(0);
+            }
+            board.push(row);
+        }
+        return board;
     }
 }
